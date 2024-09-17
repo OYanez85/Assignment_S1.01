30b89ef174afd0b45a0c510916d677de2daa8e94
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 12:14:53 +0200

    Update Editor.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/Editor.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/Editor.java
@@ -1,1 +1,54 @@
+import java.util.ArrayList;
+
+class Editor {
+    private final String name;
+    private final String dni;
+    private static double salary = 1500;
+    private ArrayList<News> newsList;
+
+    public Editor(String name, String dni) {
+        this.name = name;
+        this.dni = dni;
+        this.newsList = new ArrayList<>();
+    }
+
+    public static double getSalary() {
+        return salary;
+    }
+
+    public static void setSalary(double newSalary) {
+        salary = newSalary;
+    }
+
+    public void addNews(News news) {
+        newsList.add(news);
+    }
+
+    public void removeNews(String headline) {
+        newsList.removeIf(news -> news.getHeadline().equals(headline));
+    }
+
+    public void showNews() {
+        for (News news : newsList) {
+            System.out.println(news.getHeadline());
+        }
+    }
+
+    public News getNewsByTitle(String headline) {
+        for (News news : newsList) {
+            if (news.getHeadline().equals(headline)) {
+                return news;
+            }
+        }
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+}
  
