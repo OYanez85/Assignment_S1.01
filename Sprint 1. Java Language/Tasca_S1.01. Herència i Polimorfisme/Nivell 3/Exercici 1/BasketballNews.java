dad27f98e3d923cf0df2e43f458380388c5e97f6
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 12:11:51 +0200

    Update BasketballNews.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/BasketballNews.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/BasketballNews.java
@@ -1,1 +1,25 @@
- 
+abstract class News {
+    protected String headline;
+    protected String text = "";
+    protected double price;
+    protected int score;
+
+    public News(String headline) {
+        this.headline = headline;
+    }
+
+    public abstract double calculatePrice();
+    public abstract int calculateScore();
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+} 
