e87847d2732ee967c6b6297db0c24aad253aa2ff
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 12:14:29 +0200

    Update MotorcycleNews.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/MotorcycleNews.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/MotorcycleNews.java
@@ -1,1 +1,23 @@
+class MotorcycleNews extends News {
+    private String team;
+
+    public MotorcycleNews(String headline, String team) {
+        super(headline);
+        this.team = team;
+    }
+
+    @Override
+    public double calculatePrice() {
+        price = 100;
+        if (team.equals("Honda") || team.equals("Yamaha")) price += 50;
+        return price;
+    }
+
+    @Override
+    public int calculateScore() {
+        score = 3;
+        if (team.equals("Honda") || team.equals("Yamaha")) score += 3;
+        return score;
+    }
+}
  
