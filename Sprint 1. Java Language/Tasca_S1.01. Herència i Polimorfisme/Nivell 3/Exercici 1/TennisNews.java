94c16ea35b51a68e0657ee597f41d25597fce8c2
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 12:13:29 +0200

    Update TennisNews.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/TennisNews.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/TennisNews.java
@@ -1,1 +1,24 @@
- 
+class TennisNews extends News {
+    private String competition;
+    private String player;
+
+    public TennisNews(String headline, String competition, String player) {
+        super(headline);
+        this.competition = competition;
+        this.player = player;
+    }
+
+    @Override
+    public double calculatePrice() {
+        price = 150;
+        if (player.equals("Federer") || player.equals("Nadal") || player.equals("Djokovic")) price += 100;
+        return price;
+    }
+
+    @Override
+    public int calculateScore() {
+        score = 4;
+        if (player.equals("Federer") || player.equals("Nadal") || player.equals("Djokovic")) score += 3;
+        return score;
+    }
+} 
