665ba5f90683d618be5de36c89f4c76ac717963b
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 12:14:00 +0200

    Update F1News.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/F1News.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 3/Exercici 1/F1News.java
@@ -1,1 +1,23 @@
+class F1News extends News {
+    private String team;
+
+    public F1News(String headline, String team) {
+        super(headline);
+        this.team = team;
+    }
+
+    @Override
+    public double calculatePrice() {
+        price = 100;
+        if (team.equals("Ferrari") || team.equals("Mercedes")) price += 50;
+        return price;
+    }
+
+    @Override
+    public int calculateScore() {
+        score = 4;
+        if (team.equals("Ferrari") || team.equals("Mercedes")) score += 2;
+        return score;
+    }
+}
  
