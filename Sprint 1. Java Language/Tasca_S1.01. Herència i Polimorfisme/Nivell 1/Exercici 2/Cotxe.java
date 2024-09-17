ecea993c1b5e2efed8dd6956278559fe18d13d6d
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-17 11:30:14 +0200

    Update Cotxe.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Cotxe.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Cotxe.java
@@ -1,1 +1,24 @@
-public class Cotxe {} 
+// Cotxe.java
+public class Cotxe {
+    // Attributes
+    public static final String marca = "Volkswagen";
+    public static String model;
+    public final int potencia;
+
+    // Constructor
+    public Cotxe(String model, int potencia) {
+        Cotxe.model = model;
+        this.potencia = potencia;
+    }
+
+    // Static method
+    public static void frenar() {
+        System.out.println("El vehicle està frenant.");
+    }
+
+    // Non-static method
+    public void accelerar() {
+        System.out.println("El vehicle està accelerant.");
+    }
+}
+
