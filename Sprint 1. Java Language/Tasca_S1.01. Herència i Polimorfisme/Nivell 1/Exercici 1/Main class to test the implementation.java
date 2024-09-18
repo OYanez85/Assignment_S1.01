03b361b5b6e608199cb9daef964c4216f018351d
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-18 11:09:36 +0200

    Update Main class to test the implementation.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Main class to test the implementation.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Main class to test the implementation.java
@@ -4,14 +4,34 @@ public class Main {
         // Static block demo: Access a static member to trigger class loading
         System.out.println("Before creating instances...");
 
-        // Create instances of different instruments
+        // Create instances of different wind instruments
         WindInstrument flute = new WindInstrument("Flute", 150.0);
         flute.tocar();
 
+        WindInstrument trumpet = new WindInstrument("Trumpet", 250.0);
+        trumpet.tocar();
+
+        WindInstrument saxophone = new WindInstrument("Saxophone", 400.0);
+        saxophone.tocar();
+
+        // Create instances of different string instruments
         StringInstrument guitar = new StringInstrument("Guitar", 300.0);
         guitar.tocar();
 
+        StringInstrument violin = new StringInstrument("Violin", 500.0);
+        violin.tocar();
+
+        StringInstrument cello = new StringInstrument("Cello", 700.0);
+        cello.tocar();
+
+        // Create instances of different percussion instruments
         PercussionInstrument drum = new PercussionInstrument("Drum", 200.0);
         drum.tocar();
+
+        PercussionInstrument tambourine = new PercussionInstrument("Tambourine", 100.0);
+        tambourine.tocar();
+
+        PercussionInstrument xylophone = new PercussionInstrument("Xylophone", 350.0);
+        xylophone.tocar();
     }
 }
