f6a5f1d39d0ad93c2c7b146d90d5471ca733f196
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-18 12:12:48 +0200

    Update Main.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Main.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Main.java
@@ -6,12 +6,28 @@ public class Main {
         // Create instances of Car
         Car car1 = new Car("Golf", 150);
         Car car2 = new Car("Tiguan", 200);
+        Car car3 = new Car("Passat", 180);
+        Car car4 = new Car("Polo", 120);
+        Car car5 = new Car("T-Cross", 150);
+        Car car6 = new Car("Arteon", 320);
+        Car car7 = new Car("T-Roc", 300);
+        Car car8 = new Car("Touareg", 462);
+        Car car9 = new Car("ID.3", 204);
+        Car car10 = new Car("ID.4", 204);
 
         System.out.println("Car 1 - Model: " + Car.model + ", Horsepower: " + car1.horsepower);
         System.out.println("Car 2 - Model: " + Car.model + ", Horsepower: " + car2.horsepower);
+        System.out.println("Car 3 - Model: " + Car.model + ", Horsepower: " + car3.horsepower);
+        System.out.println("Car 4 - Model: " + Car.model + ", Horsepower: " + car4.horsepower);
+        System.out.println("Car 5 - Model: " + Car.model + ", Horsepower: " + car5.horsepower);
+        System.out.println("Car 6 - Model: " + Car.model + ", Horsepower: " + car6.horsepower);
+        System.out.println("Car 7 - Model: " + Car.model + ", Horsepower: " + car7.horsepower);
+        System.out.println("Car 8 - Model: " + Car.model + ", Horsepower: " + car8.horsepower);
+        System.out.println("Car 9 - Model: " + Car.model + ", Horsepower: " + car9.horsepower);
+        System.out.println("Car 10 - Model: " + Car.model + ", Horsepower: " + car10.horsepower);
 
         // Call static method
-        Car.breaking();
+        Car.braking();
 
         // Call non-static method
         car1.accelerating();
