e2df367588192848b404c6e5ed42f01291cd547a
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-18 11:49:14 +0200

    Update Main.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Main.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Brand: " + Car.brand);
 
-        // Create instances of Cotxe
+        // Create instances of Car
         Car car1 = new Car("Golf", 150);
         Car car2 = new Car("Tiguan", 200);
 
