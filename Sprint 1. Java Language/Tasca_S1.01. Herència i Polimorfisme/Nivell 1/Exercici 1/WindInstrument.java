e21d4c36e060de92b923ddd83309e52a7cf0cc40
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:43:20 +0200

    Update WindInstrument.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/WindInstrument.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/WindInstrument.java
@@ -1,16 +1,16 @@
-// Wind instrument class
+
 class WindInstrument extends Instrument {
     public WindInstrument(String name, double price) {
         super(name, price);
     }
 
-    // Implement the abstract tocar() method
+    
     @Override
     public void tocar() {
         System.out.println("A wind instrument is playing.");
     }
     
-    // Static block for class loading demonstration
+    
     static {
         System.out.println("WindInstrument class loaded.");
     }
