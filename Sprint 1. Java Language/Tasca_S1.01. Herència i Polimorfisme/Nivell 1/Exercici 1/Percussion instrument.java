b82eef35e26fb70c7d29757bd2c92665c4aaa33b
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:42:49 +0200

    Update Percussion instrument.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Percussion instrument.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Percussion instrument.java
@@ -1,10 +1,10 @@
-// Percussion instrument class
+
 class PercussionInstrument extends Instrument {
     public PercussionInstrument(String name, double price) {
         super(name, price);
     }
 
-    // Implement the abstract tocar() method
+    
     @Override
     public void tocar() {
         System.out.println("A percussion instrument is playing.");
