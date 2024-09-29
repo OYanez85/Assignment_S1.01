0d04bf3bcec91c5d6c6bf8f80ed07e04c3b1c114
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 20:45:45 +0200

    Update TennisNews.java

--- Nivell 3/Exercici 1_tmp/TennisNews.java
+++ Nivell 3/Exercici 1_tmp/TennisNews.java
@@ -3,19 +3,19 @@ public class TennisNews extends News {
     private String player;
     private boolean isTopPlayer;
 
-    // Constructor for String inputs (competition and player)
+    
     public TennisNews(String headline, String competition, String player) {
         super(headline);
         this.competition = competition;
         this.player = player;
 
-        // Initialize the boolean based on the player
+        
         this.isTopPlayer = player.equalsIgnoreCase("Federer") ||
                 player.equalsIgnoreCase("Nadal") ||
                 player.equalsIgnoreCase("Djokovic");
     }
 
-    // Constructor for boolean input (isTopPlayer)
+    
     public TennisNews(String headline, boolean isTopPlayer) {
         super(headline);
         this.isTopPlayer = isTopPlayer;
@@ -23,14 +23,14 @@ public class TennisNews extends News {
 
     @Override
     public double calculatePrice() {
-        double price = 150; // Base price for tennis news
+        double price = 150; 
         if (isTopPlayer) price += 100;
         return price;
     }
 
     @Override
     public int calculateScore() {
-        int score = 4; // Base score for tennis news
+        int score = 4; 
         if (isTopPlayer) score += 3;
         return score;
     }
