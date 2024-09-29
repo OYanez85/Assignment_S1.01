d7061a45e35e53d7fb3786b3804aed8842c44759
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:44:55 +0200

    Update Main.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Main.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Main.java
@@ -1,16 +1,16 @@
-// Main class to test the implementation
+
 public class Main {
     public static void main(String[] args) {
-        // Create a Smartphone object
+        
         Smartphone smartphone = new Smartphone("Samsung", "Galaxy S21");
 
-        // Call the trucar method
+        
         smartphone.trucar("123-456-789");
 
-        // Call the fotografiar method
+        
         smartphone.fotografiar();
 
-        // Call the alarma method
+        
         smartphone.alarma();
     }
 }
