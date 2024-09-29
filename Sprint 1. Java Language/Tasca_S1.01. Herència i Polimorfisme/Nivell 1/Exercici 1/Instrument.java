523b04449da6ba4e541733ca49849dcec80f1837
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:42:01 +0200

    Update Instrument.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Instrument.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/Instrument.java
@@ -1,23 +1,23 @@
-// Abstract class Instrument
+
 abstract class Instrument {
     protected String name;
     protected double price;
 
-    // Constructor to initialize name and price
+    
     public Instrument(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Abstract method to be implemented by subclasses
+    
     public abstract void tocar();
     
-    // Static block to demonstrate class loading and initialization
+    
     static {
         System.out.println("Instrument class loaded.");
     }
 
-    // Non-static initialization block (executed before the constructor)
+    
     {
         System.out.println("An instance of Instrument is being created.");
     }
