7c080b5b2216d93dd04f7d8eb5f95a20da3ef268
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:45:47 +0200

    Update Telefon.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Telefon.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Telefon.java
@@ -1,15 +1,15 @@
-// Base class Telèfon
+
 public class Telefon {
     private String marca;
     private String model;
 
-    // Constructor to initialize the brand and model
+    
     public Telefon(String marca, String model) {
         this.marca = marca;
         this.model = model;
     }
 
-    // Method to simulate making a phone call
+    
     public void trucar(String numero) {
         System.out.println("S'està trucant al número: " + numero);
     }
