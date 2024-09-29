439fde360dba07b1a7394f6d678e0cb8e5ead225
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:43:48 +0200

    Update Car.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Car.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 2/Car.java
@@ -1,21 +1,21 @@
 public class Car {
-    // Attributes
+    
     public static final String brand = "Volkswagen";
-    public String model;  // Make model non-static
+    public String model;  
     public final int horsepower;
 
-    // Constructor
+    
     public Car(String model, int horsepower) {
-        this.model = model;  // Use instance variable
+        this.model = model;  
         this.horsepower = horsepower;
     }
 
-    // Static method
+    
     public static void braking() {
         System.out.println("The vehicle is braking.");
     }
 
-    // Non-static method
+    
     public void accelerate() {
         System.out.println("The vehicle " + model + " is accelerating with " + horsepower + " horsepower.");
     }
