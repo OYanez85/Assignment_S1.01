8415b65946aab36e1e64be67957a2723321d8a0f
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 20:44:51 +0200

    Update MotorcycleNews.java

--- Nivell 3/Exercici 1_tmp/MotorcycleNews.java
+++ Nivell 3/Exercici 1_tmp/MotorcycleNews.java
@@ -2,16 +2,16 @@ public class MotorcycleNews extends News {
     private String team;
     private boolean isHondaOrYamaha;
 
-    // Constructor that takes a String team and initializes the boolean flag
+    
     public MotorcycleNews(String headline, String team) {
         super(headline);
         this.team = team;
 
-        // Initialize the boolean flag based on the team name
+        
         this.isHondaOrYamaha = team.equalsIgnoreCase("Honda") || team.equalsIgnoreCase("Yamaha");
     }
 
-    // Constructor that takes a boolean flag directly
+    
     public MotorcycleNews(String headline, boolean isHondaOrYamaha) {
         super(headline);
         this.isHondaOrYamaha = isHondaOrYamaha;
@@ -19,20 +19,20 @@ public class MotorcycleNews extends News {
 
     @Override
     public double calculatePrice() {
-        double price = 100; // Base price for Motorcycle news
+        double price = 100; 
 
-        // Use the boolean flag to determine if we add extra price for Honda/Yamaha
+        
         if (isHondaOrYamaha) price += 50;
 
         return price;
     }
 
-    // Rename this method to match the abstract method in the News class
+    
     @Override
     public int calculateScore() {
-        int score = 3; // Base score for Motorcycle news
+        int score = 3; 
 
-        // Use the boolean flag to determine if we add extra score for Honda/Yamaha
+        
         if (isHondaOrYamaha) score += 3;
 
         return score;
