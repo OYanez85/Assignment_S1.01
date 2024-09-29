ea7c7516c02bcd38106125c2852fc2a0eb3d0d7f
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:45:29 +0200

    Update Smartphone.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Smartphone.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 2/Exercici 1/Smartphone.java
@@ -1,18 +1,18 @@
-// Smartphone class that extends Telèfon and implements Camera and Rellotge
+
 public class Smartphone extends Telefon implements Camera, Rellotge {
 
-    // Constructor that calls the superclass constructor
+    
     public Smartphone(String marca, String model) {
         super(marca, model);
     }
 
-    // Implementing the fotografiar method from Camera interface
+    
     @Override
     public void fotografiar() {
         System.out.println("S'està fent una foto");
     }
 
-    // Implementing the alarma method from Rellotge interface
+    
     @Override
     public void alarma() {
         System.out.println("Està sonant l'alarma");
