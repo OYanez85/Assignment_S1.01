f4b91c3d407030b43a880412cae4909bdc06e881
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-09-29 19:43:03 +0200

    Update String instrument.java

--- Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/String instrument.java
+++ Sprint 1. Java Language/Tasca_S1.01. Herència i Polimorfisme/Nivell 1/Exercici 1/String instrument.java
@@ -1,10 +1,10 @@
-// String instrument class
+
 class StringInstrument extends Instrument {
     public StringInstrument(String name, double price) {
         super(name, price);
     }
 
-    // Implement the abstract tocar() method
+    
     @Override
     public void tocar() {
         System.out.println("A string instrument is playing.");
